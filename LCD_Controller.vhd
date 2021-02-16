a3abcb613c748832fd4299674119e5583d49bd5d
Joseph Drahos <56799491+JosephDrahos@users.noreply.github.com>
2021-02-16 17:52:07 -0500

    Update LCD_Controller.vhd

--- LCD_Controller.vhd
+++ LCD_Controller.vhd
@@ -22,13 +22,13 @@ entity LCD_Controller is
   );
   port
   (
-    I_CLK_50MHZ     : in std_logic;     -- Input clock signal
-    Reset           : in std_logic;     --Reset signals
-    data_in         : in std_logic_vector (9 downto 0);--control signals and data
-    lcd_en          : in std_logic;
-    BUSY            : out std_logic;
-    RS, E, RW       : out std_logic;
-    data_out        : out std_logic_vector (7 downto 0)
+    clk     : in std_logic;     -- Input clock signal
+    reset_n           : in std_logic;     --Reset signals
+    lcd_bus         : in std_logic_vector (9 downto 0);--control signals and data
+    lcd_enable          : in std_logic;
+    busy            : out std_logic;
+    rs, e, rw       : out std_logic;
+    lcd_data        : out std_logic_vector (7 downto 0)
   );
 end LCD_Controller;
 
@@ -36,121 +36,91 @@ architecture LUT of LCD_Controller is
   type LCD_STATE is (init, ready, send);
    signal state : LCD_STATE;
 begin
-  LCD_Controller_FSM : process (I_CLK_50MHZ, Reset)
+  LCD_Controller_FSM : process (clk, reset_n)
     variable count : integer := 0; --clock counter
     begin
-      if(rising_edge(I_CLK_50MHZ)) then
+      if(rising_edge(clk)) then
         case state is
           when init =>
-            BUSY <= '1';
+            busy <= '1';
             count := count + 1;
             --wait 50 ms on startup
             if(count < (50000*clk_freq))then
-                RS <= '0';
-                RW <= '0';
+                rs <= '0';
+                rw <= '0';
 					 state <= init;
-            --wait 100 us
-            elsif(count < (50100 * clk_freq)) then
-              data_out <= "00110000";--init for 8 bit interface
-              E <= '1';
+            --wait 10us
+            elsif(count < (50010 * clk_freq)) then
+              lcd_data <= "00111000";--init for 8 bit interface
+              e <= '1';
               state <= init;
-              --wait 4.1 ms
-            elsif(count < (54200*clk_freq)) then
-              data_out <= (others => '0');
-              E <= '0';
+              --wait 50us
+            elsif(count < (50060*clk_freq)) then
+              lcd_data <= (others => '0');
+              e <= '0';
               state <= init;
-              --wait 100us
-            elsif(count < (54300*clk_freq))then
-              data_out <= "00110000";--init for 8 bit interface
-              E <= '1';
+              --wait 10us
+            elsif(count < (50070*clk_freq))then
+              lcd_data <= "00001100";--init for 8 bit interface
+              e <= '1';
               state <= init;
-              --wait 1 ms
-            elsif(count < (55300*clk_freq)) then
-              data_out <= (others => '0');
-              E <= '0';
+              --wait 50us
+            elsif(count < (50120*clk_freq)) then
+              lcd_data <= (others => '0');
+              e <= '0';
               state <= init;
-              --wait 100us
-            elsif(count < (55400*clk_freq))then
-              data_out <= "00110000";--init for 8 bit interface
-              E <= '1';
+              --wait 10us
+            elsif(count < (50130*clk_freq))then
+              lcd_data <= "00000001";--display clear
+              e <= '1';
               state <= init;
-              --wait 1 ms
-            elsif(count < (56400*clk_freq)) then
-              data_out <= (others => '0');
-              E <= '0';
+              --wait 2 ms
+            elsif(count < (52130*clk_freq)) then
+              lcd_data <= (others => '0');
+              e <= '0';
               state <= init;
-              --wait 100us
-            elsif(count < (57500*clk_freq))then
-              data_out <= "00110000";--no lines, char font
-              E <= '1';
+              --wait 10us
+            elsif(count < (52140*clk_freq))then
+              lcd_data <= "00000110";--entry mode decrement and shift off
+              e <= '1';
               state <= init;
-              --wait 1 ms
-            elsif(count < (58500*clk_freq)) then
-              data_out <= (others => '0');
-              E <= '0';
-              state <= init;
-              --wait 100us
-            elsif(count < (58600*clk_freq))then
-              data_out <= "00001100";--display on, cursor off, no blink
-              E <= '1';
-              state <= init;
-              --wait 1 ms
-            elsif(count < (59600*clk_freq)) then
-              data_out <= (others => '0');
-              E <= '0';
-              state <= init;
-              --wait 100us
-            elsif(count < (59700*clk_freq))then
-              data_out <= "00000110";--move the cursor, do not shift the display
-              E <= '1';
-              state <= init;
-              --wait 1 ms
-            elsif(count < (60700*clk_freq)) then
-              data_out <= (others => '0');
-              E <= '0';
-              state <= init;
-              --wait 100us
-            elsif(count < (60800*clk_freq))then
-              data_out <= "10000000";--set the cursor position to home address
-              E <= '1';
-              state <= init;
-              --wait 1 ms
-            elsif(count < (61800*clk_freq)) then
-              data_out <= (others => '0');
-              E <= '0';
+              --wait 60 us
+            elsif(count < (52200*clk_freq)) then
+              lcd_data <= (others => '0');
+              e <= '0';
               state <= init;
             else
-              BUSY <= '0';
+              busy <= '0';
               count := 0;
               state <= ready;
             end if;
 
           when ready =>
-            if(lcd_en = '1') then
-              BUSY <= '1';
-              RS <= data_in(9);
-              RW <= data_in(8);
-              data_out <= data_in(7 downto 0);
+            if(lcd_enable = '1') then
+              busy <= '1';
+              rs <= lcd_bus(9);
+              rw <= lcd_bus(8);
+              lcd_data <= lcd_bus(7 downto 0);
               count := 0;
               state <= send;
             else
-              BUSY <= '0';
-              RS <= '0';
-              RW <= '0';
-              data_out <= (others => '0');
+              busy <= '0';
+              rs <= '0';
+              rw <= '0';
+              lcd_data <= (others => '0');
               state <= ready;
             end if;
 
           when send =>
-            BUSY <= '1';
-            --cant exit until 4 ms
-            if (count < (4000*clk_freq))then
+            busy <= '1';
+            --dont exit for 50us
+            if (count < (50*clk_freq))then
               if(count < clk_freq) then
-                E <= '0';
-              elsif(count < (1500*clk_freq))then
-                E <= '1';
-              elsif(count < (3000*clk_freq))then
-                E <= '0';
+                e <= '0';
+              elsif(count < (15*clk_freq))then
+                e <= '1';
+              elsif(count < (30*clk_freq))then
+                e <= '0';
               end if;
               count := count + 1;
               state <= send;
@@ -161,10 +131,9 @@ begin
 
         end case;
         --reset
-        if(Reset = '1')then
-			 data_out <= "00000001";
-          state <= init;
+        if(reset_n = '0')then
           count := 0;
+          state <= init;
         end if;
       end if;
   end process LCD_Controller_FSM;
