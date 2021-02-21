45e807be0c4e2cff91cb0612e01475d50d687f0c
siddeshsood <71221460+siddeshsood@users.noreply.github.com>
2021-02-21 20:32:06 +0530

    Update tb_LCD_Controller.vhd

--- tb_LCD_Controller.vhd
+++ tb_LCD_Controller.vhd
@@ -7,7 +7,7 @@ use IEEE.std_logic_1164.all;
 entity tb_LCD_Controller is
 end tb_LCD_Controller;
 
-architecture tb of tb_LCD_Vontroller is 
+architecture tb of tb_LCD_Controller is 
 	component LCD_Controller 
     
     generic(
@@ -15,49 +15,65 @@ architecture tb of tb_LCD_Vontroller is
   );
   port
   (
-    I_CLK_50MHZ     : in std_logic;     
-    Reset           : in std_logic;     
+    clk             : in std_logic;     
+    reset_n         : in std_logic;     
     data_in         : in std_logic_vector (9 downto 0);
-    lcd_en          : in std_logic;
+    
+    lcd_enable      : in std_logic;
     BUSY            : out std_logic;
     RS, E, RW       : out std_logic;
     data_out        : out std_logic_vector (7 downto 0)
   );
 end component;
 
-signal I_CLK_50MHZ     : in std_logic;     
-signal    Reset           : in std_logic;     
+signal    clk             : in std_logic;     
+signal    reset_n         : in std_logic;     
 signal    data_in         : in std_logic_vector (9 downto 0);
-signal    lcd_en          : in std_logic;
+signal    lcd_enable          : in std_logic;
 signal    BUSY            : out std_logic;
 signal    RS, E, RW       : out std_logic;
-signal    data_out        : out std_logic_vector (7 downto 0)
-
+signal    data_out        : out std_logic_vector; (7 downto 0)
+clock_process: process
 begin
 	
     dut : LCD_Controller
     
     port map(
-    		I_CLK_50MHZ  => I_CLK_50MHZ ;     
-    		Reset        => Reset;     
-    		data_in      => data_in;
-    		lcd_en       => lcd_en;
-    		BUSY         => BUSY;
-    		RS           => RS;
-                E            => E;
-                RW           => RW;
-    		data_out     => data_out; 
+    		clk          => clk ,     
+    		reset_n      => reset_n,     
+    		data_in      => data_in,
+    		
+            lcd_enable   => lcd_enable,
+    		BUSY         => BUSY,
+    		RS           => RS,
+            E            => E,
+            RW           => RW,
+    		data_out     => data_out);
 			
 			
 		clock_process: process
 		begin 
-			I_CLK_50MHZ <= '0';
+			clk <= '0';
 			wait for clock_period/2;
-			I_CLK_50MHZ <= '1';
+			clk <= '1';
 			wait for clock_period/2;
 		end process;
     test : process
-			begin 
+			begin
+            	
+                reset_n <= '1';
+                wait for 50 ms;
+                reset_n <= '0';
+                lcd_enable <= '0';
+                count <= '50000';
+                wait for 10 us;
+                lcd_enable <= '1';
+                wait 10 us;
+                lcd_enable <= '0';
+                
+
+
+			end process;
             
             
-        
+ end tb;
